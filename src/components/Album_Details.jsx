0f84dba43452f640c8086d5d4c6cb60dc25b466d
David <atembarry12@gmail.com>
2024-04-20 22:42:56 +0100

    Updated

--- src/components/Album_Details.jsx
+++ src/components/Album_Details.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useFetchAlbumQuery, useGetArtistAlbumsQuery } from "../loaders/apiSlice";
 import { Header } from "./Header";
+import { makeTimeString } from "./PlaylistLoad";
 
 export function Album_Info(){
     const id = useParams();
@@ -10,14 +11,21 @@ export function Album_Info(){
         <>
         <Header/>
         {data ? <>
-        <div style={{display: 'grid', gridTemplateColumns: "auto auto"}}>
+        <div style={{display: 'grid', gridTemplateColumns: "40% 50%"}}>
             <img src = {data.images[1].url} style={{borderRadius:"1.25rem"}} />
             <div>
-                <p>{data.name}</p>
-                <p>{data.artists[0].name}</p>
+                <h2>{data.name}</h2>
+                <a href={`/artists/${data.artists[0].id}`}>{data.artists[0].name}</a>
             </div>
-
-
+        </div>
+        <div style={{padding: "0.5rem"}}>
+            {data.tracks.items.map((track)=> (
+                <>
+                    <p>{data.tracks.items.indexOf(track) + 1}</p>
+                    <a href={`/song/${track.id}`}>{track.name}</a>
+                    <p>Duration: {makeTimeString(track.duration_ms)}</p>
+                    </>
+                ))}
         </div>
         </> : <>{error? <p>{error.data.error.message}</p> : <p>Loading...</p>}</>}
         </>

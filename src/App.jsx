d93d738858c6ad87b74795ea18ebac572faa8fed
David <atembarry12@gmail.com>
2024-04-30 10:39:04 +0100

    PKCE Works

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,41 @@ import { access_token, clearToken, storeToken } from "./store_slices/idSlice"
 import { Header } from "./components/Header";
 import { SearchProcessor} from "./components/SearchResults";
 
+
+const generateRandomString = (length) => {
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const values = crypto.getRandomValues(new Uint8Array(length));
+    return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+  }
+  
+  const codeVerifier  = generateRandomString(64);
+  
+  const sha256 = async (plain) => {
+    const encoder = new TextEncoder()
+    const data = encoder.encode(plain)
+    return window.crypto.subtle.digest('SHA-256', data)
+  }
+  
+  const base64encode = (input) => {
+    return btoa(String.fromCharCode(...new Uint8Array(input)))
+      .replace(/=/g, '')
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_');
+  }
+  
+  const hashed = await sha256(codeVerifier)
+  const codeChallenge = base64encode(hashed);
+
+/* scope,
+ ,
+  code_challenge: codeChallenge
+  2*/
 function App() {
   const CLIENT_ID = import.meta.env.VITE_CLIENT_ID ;
   const REDIRECT_URI = window.location.href;
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const scopes = 'user-top-read user-read-private user-read-email'
+  const CCM= 'S256'
   const RESPONSE_TYPE = "token"
   const [tokenVal, setToken] = useState("")
   const [artistName, setName] = useState("")
@@ -15,32 +46,6 @@ function App() {
   let token = useSelector(access_token)
 
 
-  /*const generateRandomString = (length) => {
-  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const values = crypto.getRandomValues(new Uint8Array(length));
-  return values.reduce((acc, x) => acc + possible[x % possible.length], "");
-}
-
-const codeVerifier  = generateRandomString(64);
-
-const sha256 = async (plain) => {
-  const encoder = new TextEncoder()
-  const data = encoder.encode(plain)
-  return window.crypto.subtle.digest('SHA-256', data)
-}
-
-const base64encode = (input) => {
-  return btoa(String.fromCharCode(...new Uint8Array(input)))
-    .replace(/=/g, '')
-    .replace(/\+/g, '-')
-    .replace(/\//g, '_');
-}
-
-const hashed = await sha256(codeVerifier)
-const codeChallenge = base64encode(hashed);
-
-
-*/
 
   useEffect(() => {
       const hash = window.location.hash
@@ -67,7 +72,7 @@ const codeChallenge = base64encode(hashed);
               {!token ?
               <>
                <div style={{width:'100%', height:'80vh', display:"grid"}}>
-                  <a style={{borderRadius:"1.5rem", color: 'black', backgroundColor:"green", fontSize:"1.5rem", placeSelf:"center", padding: '0.5rem'}} href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
+                  <a style={{borderRadius:"1.5rem", color: 'black', backgroundColor:"green", fontSize:"1.5rem", placeSelf:"center", padding: '0.5rem'}} href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${scopes}&code_challenge_method=${CCM}&code_challenge=${codeChallenge}`}>Login
                       to Spotify</a>
                       </div>
                 </>

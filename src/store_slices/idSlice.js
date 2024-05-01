89e5ac770d4cd78ec541abe7c1371cff741f753e
David <atembarry12@gmail.com>
2024-05-01 11:16:10 +0100

    Yay

--- src/store_slices/idSlice.js
+++ src/store_slices/idSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const tokenSlice = createSlice({
     name: "token",
     initialState: {
-        value : "empty"
+        value : ""
     },
     reducers:{
         storeToken: (state, action) => {

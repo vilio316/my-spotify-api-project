dbc72529514529c0855c01363a672d2a7ec368ad
David <atembarry12@gmail.com>
2024-05-01 11:07:36 +0100

    Deploy Bug Fix: Null Token

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
 
   useEffect(() => {
       const hash = window.location.hash
-      if (token.length < 1 && hash) {
+      if (token && token.length < 1 && hash) {
           token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
           window.location.hash = ""
       }
